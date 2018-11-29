5db2b4009a967ea9d44c1c6b6d32ce95358ebcad
Gonzalon <40928276+Gonzalon3140@users.noreply.github.com>
2018-11-29 15:50:50 -0800

    update

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,10 @@ var mysql = require("mysql");
 var dbconnection;
 
 if (process.env.JAWSDB_URL) {
+  console.log(process.env.JAWSDB_URL)
   dbconnection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
+  console.log("no jaws")
   dbconnection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -13,7 +15,7 @@ if (process.env.JAWSDB_URL) {
   });
 }
 
-dbconnection.connect(function(err) {
+dbconnection.connect(function (err) {
   if (err) {
     console.error("error connecting:" + err.stack);
     return;
