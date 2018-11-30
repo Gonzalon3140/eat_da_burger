b2c45288d3e405abd9c9a23ea67f6b1553f0e6f7
Gonzalon <40928276+Gonzalon3140@users.noreply.github.com>
2018-11-29 16:21:37 -0800

    update

--- config/connection.js
+++ config/connection.js
@@ -20,7 +20,20 @@ dbconnection.connect(function (err) {
     console.error("error connecting:" + err.stack);
     return;
   }
-  console.log("...connected as id " + dbconnection.threadId);
+
+  var query = 'DROP DATABASE IF EXISTS burgers_db;CREATE DATABASE burgers_db;USE burgers_db;CREATE TABLE burgers(id INT(10) NOT NULL AUTO_INCREMENT,burger_name VARCHAR(255),devoured BOOLEAN NOT NULL DEFAULT false,PRIMARY KEY(id));'
+  connection.query(query, function (err, res) {
+    if (err) {
+      throw err;
+    }
+    var query2 = 'INSERT INTO burgers(burger_name, devoured)VALUES("bacon burger", false);INSERT INTO burgers(burger_name, devoured)VALUES("double bacon swiss cheese burger", false);INSERT INTO burgers(burger_name, devoured)VALUES("whiskey river chicken burger", false);INSERT INTO burgers(burger_name, devoured)VALUES("bbq pineapple avacodo chicken burger", false);INSERT INTO burgers(burger_name, devoured)VALUES("black bean lettuce wrap protein style burger", false);INSERT INTO burgers(burger_name, devoured)VALUES("big mac", false);'
+    connection.query(query2, function (err, res) {
+      if (err) {
+        throw err;
+      }
+      console.log("...connected as id " + dbconnection.threadId);
+    })
+  })
 });
 
 module.exports = dbconnection;
